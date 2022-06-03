931f14b622a63cce4fc50f89466ddd1098be761b
shforeal <rivena14798@gmail.com>
2022-06-04 01:03:42 +0300

    'first'

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+  import todoApp from './components/todo-app.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <todoApp />
+    </div>
+  </header>
+</template>
+
+<style>
+  @import './assets/base.css';
+
+  body {
+    box-sizing: border-box;
+  }
+
+  #app {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+
+    font-weight: normal;
+  }
+
+  header {
+    line-height: 1.5;
+    text-align: center;
+  }
+
+  .logo {
+    display: block;
+    margin: 0 auto 2rem;
+  }
+
+  a,
+  .green {
+    text-decoration: none;
+    color: hsla(160, 100%, 37%, 1);
+    transition: 0.4s;
+  }
+
+  @media (hover: hover) {
+    a:hover {
+      background-color: hsla(160, 100%, 37%, 0.2);
+    }
+  }
+
+  @media (min-width: 1024px) {
+    header {
+      display: flex;
+      place-items: center;
+      /* padding-right: calc(var(--section-gap) / 2); */
+    }
+
+    .logo {
+      margin: 0 2rem 0 0;
+    }
+  }
+</style>
---
+++ src/components/todo-app.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import {reactive, ref, type Ref} from 'vue'
+
+  interface Todo {
+    id: number
+    txt: string
+    completed: boolean
+  }
+
+  const count: Ref<number> = ref(0)
+  const todos = ref([
+    {id: 0, txt: 'just do it', completed: false},
+    {id: 1, txt: 'yes you can', completed: false},
+  ])
+  function increment() {
+    count.value++
+  }
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>Hi Todos</h1>
+    <h3>{{ count }}</h3>
+    <button @click="increment">Increment</button>
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <pre>{{ todo }}</pre>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
